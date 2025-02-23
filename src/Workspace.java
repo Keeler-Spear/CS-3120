79214b64948eb9042188f24dc46a76e37817e284
Keeler Spear <keelerspear2@gmail.com>
2025-02-22 20:31:17 -0700

    commit

---
+++ src/Workspace.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+import java.util.function.Function;
+
+//Area for informal testing of my code
+public class Workspace {
+    static final int n = 20;
+    static final Function[] bFnc = BasisFunctions.trig(n);
+    static final double a = 0.01;
+
+
+    static Random rand = new Random();
+    static final int trSize = 300;
+    static final int teSize = 200;
+    static final double lowerBound = 0;
+    static final double upperBound = 3;
+    static final double aMag = 1;
+
+    private static double fnc(double x) {
+        return aMag * 2 * (rand.nextDouble() - 1) + 3 + 5 * Math.log(x) + Math.pow(x, 3) - 2 * Math.sin(x);
+
+    }
+
+    public static void main(String[] args) {
+
+        Matrix xValsTrain = LinearAlgebra.randMatrix(trSize, 1, lowerBound, upperBound);
+        Matrix xValsTest = LinearAlgebra.randMatrix(teSize, 1, lowerBound, upperBound);
+        Matrix yValsTrain = new Matrix(trSize, 1);
+        Matrix yValsTest = new Matrix(teSize, 1);
+
+        //y=0.5*X^5 - 5X^3 - X^2 +2 + a
+        for (int i = 1; i <= xValsTrain.getRows(); i++) {
+            yValsTrain.setValue(i, 1, fnc(xValsTrain.getValue(i, 1)));
+        }
+
+        for (int i = 1; i <= xValsTest.getRows(); i++) {
+            yValsTest.setValue(i, 1, fnc(xValsTest.getValue(i, 1)));
+        }
+
+        Matrix w = LinearAlgebra.zeroMatrix(n * xValsTrain.getCols() + 1,1);
+
+        Matrix min = LinReg.gradDes(xValsTrain, yValsTrain, w, n, a, bFnc);
+
+        Matrix xLin = LinearAlgebra.linSpace(lowerBound, upperBound, 0.1);
+
+        Matrix fnc = LinReg.buildFunction(xLin, min, bFnc);
+
+        double errorTr = Error.meanSquared(xValsTrain, yValsTrain, min, bFnc);
+
+        double errorTe = Error.meanSquared(xValsTest, yValsTest, min, bFnc);
+
+
+        System.out.println("Least Squares Fit for Order " + n + ":");
+        System.out.println(min);
+        System.out.println("------");
+        System.out.println("MSE of Training Data: "  + errorTr);
+        System.out.println("MSE of Testing Data: "  + errorTe);
+
+        String title = "Best Fit for Order " + String.valueOf(n);
+        PyGraph.scatterWFnc(xValsTest, yValsTest, xLin, fnc,"x", "y", title);
+    }
+}
