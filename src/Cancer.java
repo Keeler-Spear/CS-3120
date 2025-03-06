dbad9e7d67aa70d4fd01ca18e93a81a039d74e85
Keeler Spear <keelerspear2@gmail.com>
2025-03-05 17:30:17 -0700

    Commit

--- src/Cancer.java
+++ src/Cancer.java
@@ -4,40 +4,27 @@ import java.util.function.Function;
 
 public class Cancer {
 
-    public static int n = 2; //93.8%
-    public static Function[] bFnc = BasisFunctions.poly(n);
+    public static int n = 1; //93.8%
+    public static Function[] bFnc = BasisFunctions.trig(n);
     public static double a = 0.001;
 
     public static void main(String[] args) {
 
-
         String file = "C:\\Users\\grand\\PersonalProjects\\CS-3120\\data\\data.csv"; //From https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data?resource=download
-        double[][] dataVals = ReadFile.csvToArray(file, "M", "B", 1);
 
-        double[][] trData = new double[376][dataVals[0].length];
-        double[][] teData = new double[193][dataVals[0].length];
-        //Splitting data
-        for (int i = 0; i < trData.length; i++) {
-            trData[i] = dataVals[i];
-        }
-        for (int i = 0; i < teData.length; i++) {
-            teData[i] = dataVals[i + trData.length];
-        }
+        Matrix[] data = Data.getBCData(file, "M", "B", 1,66.6,true, false, false);
 
-        Matrix xTrain = new Matrix(trData);
-        Matrix yTrain = LinearAlgebra.vectorFromColumn(xTrain, 1);
-        xTrain.removeCol(1); //Removing labels
-
-        Matrix xTest = new Matrix(teData);
-        Matrix yTest = LinearAlgebra.vectorFromColumn(xTest, 1);
-        xTest.removeCol(1); //Removing labels
+        Matrix xTrain = data[0];
+        Matrix yTrain = data[1];
+        Matrix xTest = data[2];
+        Matrix yTest = data[3];
 
         Matrix w0 = LinearAlgebra.zeroMatrix(n * xTrain.getCols() + 1, 1);
 
         Matrix w = Regression.logisticReg(xTrain, yTrain, w0, a, bFnc);
 
-        Matrix CM = Error.confusionMatrix(xTest, yTest, w, bFnc);
+        Matrix CM = Metrics.confusionMatrix(xTest, yTest, w, bFnc);
 
-        Error.printClassificationReport(CM);
+        Metrics.printClassificationReport(CM);
     }
 }
